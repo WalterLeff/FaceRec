47f75e66b0987e0bb7d0343c07b81e41d832c773
wouter Lefebvre <wouter.lefebvre@hotmail.com>
2020-01-05 17:56:27 +0100

    pipeline test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World and azure and from pipelines '
+    return 'Hello World and azure and from pipelines 2 '
 
 
 if __name__ == '__main__':
